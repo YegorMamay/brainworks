b0c90e612696e5f840a76ddc86b2fe324ffca110
YegorMamay <web@brainworks.com.ua>
2019-10-01 17:24:28 +0300

    thank you page

--- page-thank-you.php
+++ page-thank-you.php
@@ -4,27 +4,14 @@
  **/
 ?>
 
-<!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js">
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-	<meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
-    
-	<title><?php wp_title(); ?></title>
-	<link rel="shortcut icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/favicon.ico" type="image/x-icon" />
-
-	<?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
+<?php get_header(); ?>
 <div class="container">
 
 <?php get_template_part('loops/content', 'page'); ?>
 
-</div><!-- /.container -->
-<?php wp_footer(); ?>
+<a href="<?php echo esc_url(home_url('/')); ?>" class="btn btn-secondary btn-lg">
+    <?php _e('Back to the homepage', 'brainworks'); ?>
+</a>
 
-</body>
-</html>
+</div><!-- /.container -->
+<?php get_footer(); ?>
