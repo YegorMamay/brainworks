0c379c3d23e600d37e9dec8176fa605e7a732029
Kudinov Fedor <brothersrabbits@mail.ru>
2019-10-03 14:36:56 +0300

    Remove unnecessary code

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,6 @@
 'use strict';
 
-const themeName = 'brainworks';
-
 import gulp from 'gulp';
-import zip from 'gulp-zip';
 import sass from 'gulp-sass';
 import babel from 'gulp-babel';
 import uglify from 'gulp-uglify';
@@ -12,21 +9,8 @@ import rename from 'gulp-rename';
 import plumber from 'gulp-plumber';
 import svgstore from 'gulp-svgstore';
 import cleancss from 'gulp-clean-css';
-import browser_sync from 'browser-sync';
 //import sourcemaps from 'gulp-sourcemaps';
 import autoprefixer from 'gulp-autoprefixer';
-
-const browserSync = browser_sync.create();
-
-const getFullDate = () => {
-    const d = new Date(),
-        year = d.getFullYear(),
-        month = d.getMonth() < 10 ? '0' + (d.getMonth() + 1) : d.getMonth() + 1,
-        date = d.getDate() < 10 ? '0' + d.getDate() : d.getDate(),
-        hours = d.getHours() < 10 ? '0' + d.getHours() : d.getHours(),
-        minutes = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
-    return `${date}.${month}.${year}_${hours}.${minutes}`;
-};
 
 gulp.task('svg', () => {
     return gulp.src('assets/img/svg/*.svg')
@@ -64,10 +48,8 @@ gulp.task('sass', () => {
 gulp.task('css', () => {
     return gulp.src('style.css')
         .pipe(plumber())
-        //.pipe(sourcemaps.init())
         .pipe(cleancss({compatibility: 'ie7', debug: true}))
         .pipe(rename({suffix: '.min'}))
-        //.pipe(sourcemaps.write('/'))
         .pipe(gulp.dest('./'));
 });
 
@@ -96,31 +78,10 @@ gulp.task('js', () => {
         .pipe(gulp.dest('./assets/js'));
 });
 
-gulp.task('zip', () => {
-    return gulp.src('**/{*,}.*', {base: '.'})
-        .pipe(plumber())
-        .pipe(zip(`${themeName}_(${getFullDate()}).zip`, {compress: true}))
-        .pipe(gulp.dest('./'));
-});
-
-gulp.task('min', gulp.parallel('css', 'js'));
-
 gulp.task('watch', () => {
     gulp.watch('assets/img/svg/*.svg', gulp.series('svg'));
     gulp.watch('assets/sass/**/*.scss', gulp.series('sass'));
     gulp.watch('assets/js/es6/**/*.js', gulp.series('babel'));
 });
 
-gulp.task('default', () => {
-    browserSync.init({
-        baseDir: './',
-        ghostMode: false,
-        //proxy: 'http://yourlocal.dev',
-    });
-    gulp.watch('**/*.php').on('change', browserSync.reload);
-    gulp.watch('style.css').on('change', browserSync.reload);
-    gulp.watch('assets/js/*.js').on('change', browserSync.reload);
-    gulp.watch('assets/img/svg/*.svg', gulp.series('svg'));
-    //gulp.watch('assets/sass/**/*.scss', gulp.series('sass'));
-    //gulp.watch('assets/js/brainworks.js', gulp.series('js'));
-});
+gulp.task('default', gulp.parallel('css', 'js'));
