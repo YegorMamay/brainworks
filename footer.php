7b8f0b14338905403d8f4931ddbebefe9ad7f376
Kudinov Fedor <brothersrabbits@mail.ru>
2019-10-03 14:22:53 +0300

    Remove not used buttons;

--- footer.php
+++ footer.php
@@ -24,18 +24,6 @@
 
 <?php scroll_top(); ?>
 
-<?php if (is_customize_preview()) { ?>
-    <button class="btn btn-secondary btn-sm customizer-edit" data-control='{ "name":"bw_scroll_top_display" }'>
-        <?php esc_html_e('Edit Scroll Top', 'brainworks'); ?>
-    </button>
-    <button class="btn btn-secondary btn-sm customizer-edit" data-control='{ "name":"bw_analytics_google_placed" }'>
-        <?php esc_html_e('Edit Analytics Tracking Code', 'brainworks'); ?>
-    </button>
-    <button class="btn btn-secondary btn-sm customizer-edit" data-control='{ "name":"bw_login_logo" }'>
-        <?php esc_html_e('Edit Login Logo', 'brainworks'); ?>
-    </button>
-<?php } ?>
-
 <div class="is-hide"><?php svg_sprite(); ?></div>
 
 <?php wp_footer(); ?>
