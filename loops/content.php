3fe5d9369de845cd39da15e07f925ac0bf67568b
YegorMamay <web@brainworks.com.ua>
2019-10-16 22:54:09 +0300

    articles markup changes

--- loops/content.php
+++ loops/content.php
@@ -2,17 +2,13 @@
     <div class="js-ajax-posts">
         <?php while (have_posts()): the_post(); ?>
 
-                <div class="row">
-                    <article id="post_<?php the_ID() ?>">
+                    <article id="post_<?php the_ID() ?>" class="row">
                         <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-                            <section>
                                 <?php the_post_thumbnail('large'); ?>
-                            </section>
                         </div>
                         <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-                            <header>
                                 <h3><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h3>
-                                <div class="sp-xs-1 sp-sm-1 sp-md-1 sp-lg-1 sp-xl-1"></div>
+                                <div class="sp-xs-1"></div>
                                 <p><?php the_excerpt(); ?></p>
                                 <?php /*
                         <h5>
@@ -21,27 +17,20 @@
                             <time  class="text-muted" datetime="<?php the_time('d-m-Y')?>"><?php echo get_option( 'date_format' ) ?></time>
                           </em>
                         </h5>
-                        */ ?>
-                            </header>
-                            <?php /*
-                    <footer>
                         <p class="text-muted">
                             <i class="fa fa-folder-open-o"></i>&nbsp; <?php _e('Category', 'brainworks'); ?>: <?php the_category(', ') ?><br/>
                             <i class="fa fa-comment-o"></i>&nbsp; <?php _e('Comments', 'brainworks'); ?>: <?php comments_popup_link(__('None', 'brainworks'), '1', '%'); ?>
                         </p>
-                    </footer>
                     */ ?>
-                            <div class="sp-xs-2 sp-sm-2 sp-md-2 sp-lg-2 sp-xl-2"></div>
+                            <div class="sp-xs-2"></div>
                             <a class="btn btn-secondary btn-sm"
                                href="<?php echo get_permalink(); ?>"><?php _e('Continue reading', 'brainworks') ?> <i
                                         class="glyphicon glyphicon-arrow-right"></i></a>
                         </div>
                     </article>
-                </div>
-
-            <div class="sp-xs-2 sp-sm-2 sp-md-2 sp-lg-2 sp-xl-2"></div>
+            <div class="sp-xs-2"></div>
             <hr>
-            <div class="sp-xs-2 sp-sm-2 sp-md-2 sp-lg-2 sp-xl-2"></div>
+            <div class="sp-xs-2"></div>
         <?php endwhile; ?>
     </div>
 
