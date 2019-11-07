75b41a5859f2e78bd12d72cd004dc7422ca4bcfe
YegorMamay <yegor19.05@gmail.com>
2019-11-07 13:19:20 +0200

    og img url

--- header.php
+++ header.php
@@ -7,20 +7,22 @@
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-title" content="<?php bloginfo('name'); ?> - <?php bloginfo('description'); ?>">
 
+    <!-- OpenGraph -->
     <meta property="og:locale" content="ru_RU" />
     <meta property="og:locale:alternate" content="ru_RU" />
     <meta property="og:type" content="website" />
     <meta property="og:title" content="<?php bloginfo('name'); ?>" />
     <meta property="og:description" content="<?php bloginfo('description'); ?>">
-    <meta property="og:url" content="<?php site_url() ?>" />
+    <meta property="og:url" content="<?php echo esc_url(site_url()); ?>" />
     <meta property="og:site_name" content="<?php bloginfo('name'); ?>" />
-    <meta property="og:image" content="/wp-content/uploads/cover.jpg" />
-    <meta property="og:image:secure_url" content="/wp-content/uploads/cover.jpg" />
+    <meta property="og:image" content="<?php echo esc_url(the_post_thumbnail_url()); ?>" />
+    <meta property="og:image:secure_url" content="<?php echo esc_url(the_post_thumbnail_url()); ?>" />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="628" />
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:title" content="<?php bloginfo('name'); ?> | <?php bloginfo('description'); ?>" />
-    <meta name="twitter:image" content="/wp-content/uploads/cover.jpg" />
+    <meta name="twitter:image" content="<?php echo esc_url(the_post_thumbnail_url()); ?>" />
+    <!-- OpenGraph end-->
 
     <link rel="shortcut icon" href="<?php echo esc_url(get_template_directory_uri() . '/assets/img/favicon.ico'); ?>"
           type="image/x-icon">
