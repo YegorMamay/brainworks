0753edd8fa6e069c4b9035a581727cfe230c0262
YegorMamay <yegor19.05@gmail.com>
2020-02-26 23:08:51 +0200

    if sidebar

--- archive-sn_catalogs.php
+++ archive-sn_catalogs.php
@@ -10,7 +10,7 @@
     
     <div class="row">
         <div class="col-12 col-md-3">
-            <?php get_sidebar(); ?>
+            <?php dynamic_sidebar('sidebar-widget-area2'); ?>
         </div>
         
         <div class="col-12 col-sm-12 col-md-9 col-lg-9 col-xl-9">
--- single.php
+++ single.php
@@ -1,15 +1,23 @@
 <?php get_header(); ?>
 
+<?php $column_class = ''; ?>
+<?php if (is_active_sidebar('sidebar-widget-area')) { ?>
+    <?php $column_class = 'col-12 col-sm-12 col-md-9 col-lg-9 col-xl-9' ?>
+<?php } else { ?>
+    <?php $column_class = 'col-12' ?>
+<?php }  ?>
 <div class="container">
-<div class="row">
-    <div class="col-12 col-sm-12 col-md-9 col-lg-9 col-xl-9">
-        <?php if (!is_front_page() && function_exists('kama_breadcrumbs')) kama_breadcrumbs(' » '); ?>
-        <?php get_template_part('loops/content', 'single'); ?>
+    <div class="row">
+        <div class="<?php echo $column_class; ?>">
+            <?php if (!is_front_page() && function_exists('kama_breadcrumbs')) kama_breadcrumbs(' » '); ?>
+            <?php get_template_part('loops/content', 'single'); ?>
+        </div>
+        <?php if (is_active_sidebar('sidebar-widget-area')) { ?>
+            <div class="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
+                <?php get_sidebar(); ?>
+            </div>
+        <?php } ?>
     </div>
-    <div class="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
-        <?php get_sidebar(); ?>
-    </div>
-</div><!-- /.row -->
-</div><!-- /.container -->
+</div>
 
 <?php get_footer(); ?>
