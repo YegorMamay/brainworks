54866eeb0cb3bf493f8c5914b2a5c8a8667227f7
YegorMamay <web@brainworks.com.ua>
2020-03-31 12:05:30 +0300

    catalog pagination

--- archive-sn_catalogs.php
+++ archive-sn_catalogs.php
@@ -18,16 +18,32 @@
                 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                 <div class="col-12 col-sm-12 col-md-4">
                    <div class="catalog-item">
-                    <div><a href="<?php the_permalink(); ?>" class="image-catalogs"><?php the_post_thumbnail('medium'); ?></a></div>
+                    <div><a href="<?php the_permalink(); ?>" class="image-catalogs"><?php the_post_thumbnail('large'); ?></a></div>
                     <h6 class="text-center"><a href="<?php the_permalink(); ?>" class="title-catalogs"><?php the_title(); ?></a></h6>
                     </div>
                     <div class="sp-xs-3"></div>
-                </div>
+                </div>  
                 <?php endwhile;
                     else : ?>
                 <?php get_template_part('loops/content', 'none'); ?>
                 <?php endif; ?>
             </div>
+            
+            <?php if (get_theme_mod('bw_load_more_enable') && function_exists('bw_load_more')) { ?>
+        <div class="text-center"><?php bw_load_more(); ?></div>
+    <?php } else {
+        if (function_exists('bw_pagination')) {
+            bw_pagination();
+        } else {
+            if (is_paged()) { ?>
+                <ul class="pagination">
+                    <li class="older"><?php next_posts_link('<i class="fa fa-arrow-left"></i> ' . __('Previous', 'brainworks')) ?></li>
+                    <li class="newer"><?php previous_posts_link(__('Next', 'brainworks') . ' <i class="fa fa-arrow-right"></i>') ?></li>
+                </ul>
+            <?php }
+        }
+    } ?>
+            
         </div>
 
     </div><!-- /.row -->
