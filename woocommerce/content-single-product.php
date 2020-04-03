554a8b8dbb0781fb8c4d9091994ef946b679de74
YegorMamay <web@brainworks.com.ua>
2020-04-03 11:04:43 +0300

    woocommerce upgrade

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -1,14 +1,12 @@
 <?php
-
 defined( 'ABSPATH' ) || exit;
 global $product;
 do_action( 'woocommerce_before_single_product' );
 
 if ( post_password_required() ) {
 	echo get_the_password_form();
-	return; } ?>
-	
-<div class="sp-xs-3"></div>
+	return; } 
+?>
 
 <div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
     <div class="container">
