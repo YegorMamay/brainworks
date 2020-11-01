8bdff860c9f1b34e660e357349cdc7d254c1e982
YegorMamay <web@brainworks.com.ua>
2020-11-01 16:07:18 +0200

    The function "the_lang_class" not needed any more

--- single-catalog.php
+++ single-catalog.php
@@ -17,7 +17,7 @@
 			<div class="vh-xs-2"></div>
 			<div class="prices"><?php the_excerpt(); ?></div>
 			<div class="vh-xs-2"></div>
-			<button class="btn btn-secondary <?php the_lang_class('one-click'); ?>"><?php _e('Quick order', 'brainworks'); ?></button>
+			<button class="btn btn-secondary one-click"><?php _e('Quick order', 'brainworks'); ?></button>
 		</div>
         <div class="col-12">
             <div class="vh-xs-2"></div>
