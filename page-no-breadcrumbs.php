2e0bebfdeae0180ce40d55d60109920f79fa9300
Mac <mac@YegorMamay.local>
2021-05-12 20:38:45 +0300

    updated

--- page-no-breadcrumbs.php
+++ page-no-breadcrumbs.php
@@ -9,7 +9,7 @@
 
 <?php if (have_posts()): while (have_posts()): the_post(); ?>
 
-    <h1 class="page-name"><?php the_title() ?></h1>
+    <?php /*<h1 class="page-name"><?php the_title() ?></h1>*/ ?>
 
     <?php the_content() ?>
     <?php wp_link_pages(); ?>
