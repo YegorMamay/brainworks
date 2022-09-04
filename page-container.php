aad3e81c6dbd5e71d56cfb43fd32fd0192a263b4
YegorMamay <yegor19.05@gmail.com>
2022-09-04 09:27:40 +0300

    Container page

---
+++ page-container.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: Container
+ **/
+?>
+
+<?php get_header(); ?>
+<div class="container">
+
+<?php get_template_part('loops/content', 'home'); ?>
+
+</div>
+<?php get_footer(); ?>
