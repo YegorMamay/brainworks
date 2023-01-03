4ae8d78d5857d8cb2b661a1bc556aa9c6cdaf6b5
YegorMamay <yegor19.05@gmail.com>
2023-01-03 12:41:32 +0200

    update

--- woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
+++ woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -27,7 +27,7 @@ global $product;
     ?>
 
     <button type="submit" class="single_add_to_cart_button btn btn-one"><i class="fal fa-cart-arrow-down"></i>  <?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
-    <button type="button" class="single_buy_one_click_button btn btn-outline-two one-click"><?php _e('Buy in one click', 'brainworks'); ?></button>
+    <button type="button" class="single_buy_one_click_button btn btn-outline-two one-click <?php the_lang_class('one-click'); ?>"><?php _e('Buy in one click', 'brainworks'); ?></button>
 
     <?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
 
