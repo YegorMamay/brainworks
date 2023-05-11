24f87592bc2884584565a418e0cf244468f3de09
YegorMamay <yegor19.05@gmail.com>
2023-05-11 10:43:47 +0300

    update

--- woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
+++ woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -10,6 +10,9 @@
 defined( 'ABSPATH' ) || exit;
 
 global $product;
+
+$sku = $product->get_sku();
+
 ?>
 <div class="woocommerce-variation-add-to-cart variations_button">
     <?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
@@ -27,7 +30,7 @@ global $product;
     ?>
 
     <button type="submit" class="single_add_to_cart_button btn btn-one"><i class="fal fa-cart-arrow-down"></i>  <?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
-    <button type="button" class="single_buy_one_click_button btn btn-outline-two one-click <?php the_lang_class('one-click'); ?>"><?php _e('Buy in one click', 'brainworks'); ?></button>
+    <button type="button" data-title="<?php echo $product->get_name(); ?>" data-sku="<?php echo $sku; ?>" class="single_buy_one_click_button btn btn-outline-two <?php the_lang_class('one-click'); ?>"><?php _e('Buy in one click', 'brainworks'); ?></button>
 
     <?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
 
